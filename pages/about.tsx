54490286ab4bfd3d25580cf270fddabd123a5a52
Felipe Kawassaki <fekawassaki@gmail.com>
2020-12-03 08:36:21 -0300

    Time

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,6 @@ const About: React.FC = () => {
   return (
     <div>
       <h1>About</h1>
-      
       <Link href="/"><a>Home</a></Link>
     </div>
   );
---
+++ pages/tempo.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const Tempo: React.FC = ({staticDateString}) => {
+  const dynamicDate = new Date();
+  const dynamicDateString = dynamicDate.toUTCString();
+
+  return (
+    <div>
+      <h1>{dynamicDateString} (dinâmico)</h1>
+      <h1>{staticDateString} (dinâmico)</h1>
+    </div>
+  );
+}
+
+export const getStaticProps = () => {
+  const staticDate = new Date();
+  const staticDateString = staticDate.toUTCString();
+
+  return {
+    props: {
+      staticDateString
+    }
+  };
+}
+
+export default Tempo;
