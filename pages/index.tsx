016975dc3a617aad7f36a9cdd783f84e34875140
Felipe Kawassaki <fekawassaki@gmail.com>
2020-12-03 09:11:34 -0300

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Lo linda e perfect</h1>
       <Link href="/about"><a>About</a></Link>
     </div>
   );
