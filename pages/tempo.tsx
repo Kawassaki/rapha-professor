3733efca5782f568dc3c3326ed3a2d60c1d0040b
Felipe Kawassaki <fekawassaki@gmail.com>
2020-12-03 08:43:50 -0300

    delay test

--- pages/tempo.tsx
+++ pages/tempo.tsx
@@ -7,12 +7,17 @@ const Tempo: React.FC<{staticDateString: string}> = ({staticDateString}) => {
   return (
     <div>
       <h1>{dynamicDateString} (dinâmico)</h1>
-      <h1>{staticDateString} (dinâmico)</h1>
+      <h1>{staticDateString} (estático - com delay)</h1>
     </div>
   );
 }
 
-export const getStaticProps = () => {
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+export const getStaticProps = async () => {
+  console.log('Add delay of 5 seconds');
+  await delay(5000);
+
   const staticDate = new Date();
   const staticDateString = staticDate.toUTCString();
 
