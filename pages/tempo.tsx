a9de25fa96fcc426d080811397c12f0a0bf3bae8
Felipe Kawassaki <fekawassaki@gmail.com>
2020-12-03 08:45:58 -0300

    Revalidate

--- pages/tempo.tsx
+++ pages/tempo.tsx
@@ -7,24 +7,22 @@ const Tempo: React.FC<{staticDateString: string}> = ({staticDateString}) => {
   return (
     <div>
       <h1>{dynamicDateString} (dinâmico)</h1>
-      <h1>{staticDateString} (estático - com delay)</h1>
+      <h1>{staticDateString} (estático)</h1>
     </div>
   );
 }
 
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 
 export const getStaticProps = async () => {
-  console.log('Add delay of 5 seconds');
-  await delay(5000);
-
   const staticDate = new Date();
   const staticDateString = staticDate.toUTCString();
 
   return {
     props: {
       staticDateString
-    }
+    },
+    revalidate: 1
   };
 }
 
